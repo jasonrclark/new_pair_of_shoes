7d2b708d3f8ab28f629277c3c534d496f3acf1ee
Jason R. Clark <jclark@newrelic.com>
2014-11-18 12:51:10 -0800

    RUBYCONF!!!

---
+++ slides/lightning.rb
@@ -0,0 +1,28 @@
+slide "Gaga" do
+  center_horizontally image("gaga.png", height: app.height)
+end
+
+slide "RubyConfThanks" do
+  background cornflowerblue
+  centered_title 'Thanks!',
+    size: 120,
+    weight: 'bold',
+    margin_top: 100
+  para "@jasonrclark",
+    align: 'center',
+    size: 32,
+    margin_top: 30
+  para "BOF Session Tonight, 8pm Room 23",
+    align: 'center',
+    size: 48,
+    margin_top: 30
+  para "http://github.com/shoes/shoes4",
+    align: 'center',
+    size: 32,
+    margin_top: 30
+  para "http://github.com/jasonrclark/get_your_shoes_back_on",
+    align: 'center',
+    size: 32,
+    margin_top: 30
+  center_horizontally image "images/shoes.png", margin_top: 30
+end
