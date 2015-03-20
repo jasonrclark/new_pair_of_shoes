d84f1de96230a7c56df291fe22fbee2174d8d776
Tobias Pfeiffer <tobi@bitcrowd.net>
2015-03-20 23:33:55 +0100

    Get rid off para hack

--- slides/slipping_into_shoes.rb
+++ slides/slipping_into_shoes.rb
@@ -2,10 +2,7 @@ main_section "SlippingIntoShoes", 'Slipping into Shoes'
 tobi_main_section "TobiSlippingIntoShoes", 'Slipping into Shoes'
 
 example_code "ShoesApp", 'Shoes.app', "code/shoes_app.rb"
-example_code "Para",     'para',      "code/para.rb" do
-  # Sneaky, know we want this bumped up after this point
-  Shoes::Para::STYLES[:size] = 48
-end
+example_code "Para",     'para',      "code/para.rb"
 
 example_code "Button",   'button',    "code/button.rb"
 example_code "Edit",     'edit',      "code/edit.rb"
