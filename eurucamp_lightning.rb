97e51bfbfaf85328171a0f03e6c19b8fbbbc6e0c
Jason R. Clark <jclark@newrelic.com>
2015-08-03 06:04:29 -0700

    Eurucamp lightning talk

---
+++ eurucamp_lightning.rb
@@ -0,0 +1,18 @@
+wingtips_options app: {
+                   title: "Get Your Shoes (Back) On",
+                   width: 1024, height: 768,
+                   fullscreen: false }
+
+
+slides MainTitle,
+  WhatIsShoes, Why, HacketyHack, WhysGone,
+
+  Shoes4, UseJRuby,
+
+  DEMOS, Clock, Minesweeper, Snake, TobiSimpleFace,
+
+  NewcomerFriendly,
+  Gaga,
+
+  CoralineIsAwesome,
+  Thanks
